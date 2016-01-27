a1b07946b372828ec3fbed038dd1891260ae8ea7
HAB <kissahen@gmail.com>
2016-01-27 11:31:42 +0100

    Made own udp connection

--- oving3/oving3.go
+++ oving3/oving3.go
@@ -18,42 +18,27 @@ func main(){
 
 package main 
 
-
 import (
 	"fmt"
-	"github.com/holwech/heislab/udp"
-	"time"
+	"net"
 )
 
-func print_udp_message(msg udp.Udp_message){
-	fmt.Printf("msg:  \n \t raddr = %s \n \t data = %s \n \t length = %v \n", msg.Raddr, msg.Data, msg.Length)
+func udp_receive(rChan chan []byte){
+	baddr, _ := net.ResolveUDPAddr("udp4", "255.255.255.255:"+"30000")
+	broadcastListenConn, _ := net.ListenUDP("udp", baddr)
+
+	for{
+		buf := make([]byte, 1024)
+		broadcastListenConn.ReadFromUDP(buf)
+		fmt.Println(string(buf))
+		rChan <- buf
+	}
+	
 }
 
-func node (send_ch, receive_ch chan udp.Udp_message){
-for {
-	time.Sleep(1*time.Second)
-	snd_msg := udp.Udp_message{Raddr:"broadcast", Data:"Hello Fuckers", Length:11}
-	fmt.Printf("Sending------\n")
-	send_ch <- snd_msg
-	print_udp_message(snd_msg)
-	fmt.Printf("Receiving----\n")
-	rcv_msg:= <- receive_ch
-	print_udp_message(rcv_msg)
-	}
-}
 
 
 func main (){
-	send_ch := make (chan udp.Udp_message)
-	receive_ch := make (chan udp.Udp_message)
-	err := udp.Udp_init(20002, 30000, 1024, send_ch, receive_ch)	
-	go node(send_ch, receive_ch)
-
-
-	if (err != nil){
-		fmt.Print("main done. err = %s \n", err)
-	}
-		neverReturn := make (chan int)
-	<-neverReturn
-
+	rChan := make(chan []byte)
+	udp_receive(rChan);
 }
