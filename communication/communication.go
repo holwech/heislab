aec3011a96a5870221babffafafc16358fed6b6e
Joachim Holwech <joachim.holwech@gmail.com>
2016-01-28 16:13:29 +0100

    What

--- communication/communication.go
+++ communication/communication.go
@@ -1,7 +1,41 @@
 package communication
 
+import (
+	"time"
+	"net"
+	"fmt"
+)
 
+func Broadcast() {
+	fmt.Println("=== Broadcasting ===")
+	broadcastAddress, _ := net.ResolveUDPAddr("udp4", "129.241.187.255:20000")
+	localAddress := getLocalAddress(broadcastAddress)
+	connection, _ := net.DialUDP("udp4",localAddress, broadcastAddress)
+	defer connection.Close()
+	for {
+		connection.Write([]byte("Penis?"))
+		time.Sleep(1*time.Second)
+	}
+}
 
-func() Broadcast() {
-	bAddr, _ = net.ResolveUDPAddr("udp4", )
+func Listen(InputUDP chan string) {
+	fmt.Println("=== Listening ===")
+	broadcastAddress, _ := net.ResolveUDPAddr("udp4", "129.241.187.255:20001")
+	localAddress := getLocalAddress(broadcastAddress)
+	connection, _ := net.ListenUDP("udp4", localAddress)
+	defer connection.Close()
+	for{
+		buffer := make([]byte, 1024)
+		connection.ReadFromUDP(buffer)
+		InputUDP <- string(buffer)
+	}
+}
+
+func getLocalAddress(receiveAddress *net.UDPAddr) (*net.	UDPAddr) {
+	tempConnection, _ := net.DialUDP("udp4", nil, receiveAddress)
+	defer tempConnection.Close()
+	tempAddress := tempConnection.LocalAddr()
+	localAddress, _ := net.ResolveUDPAddr("udp4", tempAddress.String())
+	localAddress.Port = 20000
+	return localAddress
 }
