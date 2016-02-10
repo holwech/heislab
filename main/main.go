b01f88b0c3577f6fce589e1870e968dedf3c23dd
Joachim Holwech <joachim.holwech@gmail.com>
2016-02-10 14:31:21 +0100

    Did major changes bugfixes

--- main/main.go
+++ main/main.go
@@ -1,91 +1,37 @@
-dpackage main
+package main
 
 import (
-<<<<<<< HEAD
-	"github.com/holwech/heislab/driver"
-	"github.com/holwech/heislab/types"
+	"github.com/holwech/heislab/elevator"
 	"fmt"
 	"github.com/holwech/heislab/communication"
-	"time"
 )
 
 func main() {
-	request := make(chan int,1)
-	request <- 3
-	go RunElevator(request)
-	neverstop := make(chan int)
-	<-neverstop
-}
-
-func InitElevator()(innerChan chan types.InnerOrder,outerChan chan types.OuterOrder, floorChan chan int){
-	innerChan = make(chan types.InnerOrder)
-	outerChan = make(chan types.OuterOrder)
-	floorChan = make(chan int)
-
-	driver.InitHardware()
-	driver.SetMotorDirection(-1)
-
-	go driver.ReadInnerPanel(innerChan)
-	go driver.ReadOuterPanel(outerChan)
-	go driver.ReadFloorSensor(floorChan)
-
+	connectionData := make(chan communication.ConnData)
+	communicationData := make(chan communication.CommData)
+	elevatorData := make(chan elevator.ElevData)
+	communication.Init("skal fikses", communicationData)
+	elevator.Init(elevatorData)
 	for{
-		floorVal := <-floorChan
-		if floorVal != -1{
-			driver.SetMotorDirection(0)
-			return
-		}
-
-	}
-}
-
-func RunElevator(newRequest chan int){
-	innerChan,outerChan, floorChan := InitElevator()
-	fmt.Println("Init complete")
-
-	var state types.ElevatorState
-	for{
-		select{
-			case inner := <- innerChan:
-				fmt.Println(inner)
-
-			case outer := <- outerChan:
-				newRequest <- outer.Floor
-
-			case floor := <-floorChan:
-				if floor == -1{
-					state.IsInFloor = false
-				}else{
-					state.Floor = floor
-					state.IsInFloor = true
-					if state.Floor == state.Request{
-						driver.SetMotorDirection(0)
-					} 
-				}
-
-			case request := <- newRequest:
-				state.Request = request
-				if state.Floor < state.Request {
-					driver.SetMotorDirection(1)
-				}else if state.Floor > state.Request{
-					driver.SetMotorDirection(-1)
-				}
+		select {
+			case elevatorInput := <- elevatorData
+				handleElevInput(elevatorInput)
+			case dataInput := <- communicationData
+				handleDataInput(dataInput)
+			case connectionInput := <- connectionData
+				handleConnectionInput(connectionInput)
 		}
 	}
 }
 
-func communicationTest() {
-	data := map[string]interface{} {
-		"LOL": 1,
-		"FAKA U BTCH": "U EAT MY NUDLS",
-	}
-	receiveChannel := make(chan communication.UDPData)
-	sendChannel := make(chan communication.UDPData)
-	communication.Init("10.20.78.108", receiveChannel, sendChannel)
-	time.Sleep(1*time.Second)
-	communication.Send("10.20.78.108", data, sendChannel)
-	for {
-		message := <- receiveChannel
-		communication.PrintMessage(&message)
-	}
+func handleElevInput(input elevator.ElevData) {
+	fmt.Printf("Input type: %s, input value: %d", input.InputType)
+}
+
+func handleDataInput(data communication.CommData) {
+	fmt.Printf("Input type: %s, input value: %d", input.InputType)	
+}
+
+func handleConnectionInput(connectionData communication.ConnData) {
+	fmt.Printf("Input type: %s, input value: %d", input.InputType)
 }
