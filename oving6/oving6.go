c5d74791150f3fa3acc0e8c9d869f8f4a81ddbfd
Adrian Meidell Fiorito <adrianmfi@hotmail.com>
2016-02-21 09:23:35 +0100

    Oving 6 now makes its own backup

--- oving6/oving6.go
+++ oving6/oving6.go
@@ -5,59 +5,108 @@ import (
 	"os"
 	"time"
 	"fmt"
+	"strings"
+	"math/rand"
+	"os/exec"
 )
 
 
-func udpClient(){
-	service := "localhost:28000"
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
 
+func checkAlive(quit <-chan bool) <-chan bool{
+
+	timeout := make(chan bool)
+	go func(){
+		address := "localhost:28000"
+		udpAddr, err := net.ResolveUDPAddr("udp4", address)
+		checkError(err)
+		conn, err := net.ListenUDP("udp", udpAddr)
+		defer conn.Close()
+		checkError(err)
+		conn.SetReadDeadline(time.Now().Add(1*time.Second))
+		
+		var buf [8]byte
+		for{
+			select{
+			case <- quit:
+				close(timeout)
+				return
+			default:
+				n, _, err := conn.ReadFromUDP(buf[0:])
+				if nerr, ok := err.(net.Error); ok && nerr.Timeout(){
+					select {
+						case timeout <- true:
+						default:
+					}
+				}else if strings.Compare("alive",string(buf[0:n])) == 0{	
+						conn.SetReadDeadline(time.Now().Add(1*time.Second))
+				}
+			}
+		}
+	}()
+	return timeout
+}
+
+func pingAlive(quit <-chan bool){
+	service := "localhost:28000"
 	udpAddr, err := net.ResolveUDPAddr("udp4", service)
 	checkError(err)
-
 	conn, err := net.DialUDP("udp", nil, udpAddr)
+	defer conn.Close()
 	checkError(err)
 
 	for{
-		_, err = conn.Write([]byte("Client message"))
-		checkError(err)
+		select{
+		case <-quit:
+			return
+		default:
+			_, err = conn.Write([]byte("alive"))
+			checkError(err)
+			time.Sleep(10*time.Millisecond)	
+		}
+	}
+}	
 
-		var buf [1024]byte
-		n, err := conn.Read(buf[0:])
-		checkError(err)
 
-		fmt.Println("Msg received in udpClient:", string(buf[0:n]))
+func primary(){
+	for i := 1;	i < 1000; i++{
+		fmt.Println(i)
 		time.Sleep(1*time.Second)
 	}
 }
 
-func udpServer(){
-	service := "localhost:28000"
-	udpAddr, err := net.ResolveUDPAddr("udp4", service)
-	checkError(err)
+func randomExit(duration int){
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	conn, err := net.ListenUDP("udp", udpAddr)
-	checkError(err)
-	for {
-
-		var buf [1024]byte
-		length, addr, err := conn.ReadFromUDP(buf[0:])
-		if err != nil {
-			fmt.Println("error reading from udp")
+	for{
+		i := rand.Int()
+		if (i) % (duration) == 0{
+			fmt.Println("Random exit")
+			os.Exit(1)
 		}
-
-		fmt.Println("Msg received in udpServer: ",string(buf[0:length]))
-
-		reply := "Server msg"
-		conn.WriteToUDP([]byte(reply), addr)
+	
+		time.Sleep(1*time.Second)
 	}
 }
 
-func main() {
-	go udpServer()
-	time.Sleep(1*time.Second)
-	go udpClient()
+func main(){
+	quitCheck := make(chan bool)
+	timeout := checkAlive(quitCheck)
 
-	neverStop := make(chan int)
-	<- neverStop
+	quitCheck <-<- timeout
+
+	quitPing := make(chan bool)
+	go pingAlive(quitPing)
+	go primary()
+
+    cmd := exec.Command("bash", "-c", "start go run oving6.go")
+    cmd.Start()
+
+    neverExit := make(chan bool)
+    <-neverExit
 }
-
