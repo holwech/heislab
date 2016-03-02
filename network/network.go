061b3454967594b73d1ca74bb3ba66fbabaca350
Joachim Holwech <joachim.holwech@gmail.com>
2016-03-02 17:48:43 +0100

    Added a network struct for storage of channels

--- network/network.go
+++ network/network.go
@@ -1,5 +1,47 @@
 package network
 
+import (
+	"github.com/holwech/heislab/communication"
+)
+
 type Message struct {
 	Sender, Receiver, ID, Response, Content string
+}
+type Network struct {
+	slaveReceive, slaveStatus, slaveSend, masterReceive, masterStatus, masterSend chan Message
+}
+
+func (nw *Network)Init(slaveSend chan Message, masterSend chan Message) {
+	nw.slaveReceive := make(chan Message)
+	nw.slaveStatus := make(chan Message)
+	nw.slaveSend := slaveSend
+	nw.masterReceive := make(chan Message)
+	nw.masterStatus := make(chan Message)
+	nw.masterSend := masterSend
+}
+
+func (nw *Network) SChannels() (<- chan Message, <- chan Message){
+	return nw.slaveReceive, nw.slaveStatus
+}
+
+
+func (nw *Network) MChannels() (<- chan Message, <- chan Message){
+	return nw.masterReceive, nw.masterStatus
+}
+
+
+func Run(nw *Network) {
+	commSend := make(chan CommData)
+	commReceive, commStatus := communication.Run()
+	go sorter(nw, commSend, commReceive, commStatus)
+}
+
+
+func sorter(nw *Network, commSend chan<- communication.CommData, commReceive <-chan communication.CommData, commStatus <-chan communication.ConnData) {
+	for{
+		select{
+		case message <- nw.slaveSend:
+		case message <- nw.masterSend:
+		}
+	}
 }
