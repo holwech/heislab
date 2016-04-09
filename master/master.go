b00568640c7ce40bf0a939188c91dd964aef5039
sanntid <student@stud.ntnu.no>
2016-04-09 16:43:46 +0200

    works a little bit more

--- master/master.go
+++ master/master.go
@@ -4,7 +4,6 @@ import (
 	"github.com/holwech/heislab/network"
 	"github.com/holwech/heislab/cl"
 	"github.com/holwech/heislab/orders"	
-	"time"
 	"fmt"
 )
 
@@ -19,51 +18,64 @@ func Run(nw *network.Network, sendMaster chan network.Message){
 	messageChan, statusChan := nw.MChannels()
 	sys := orders.NewSystem()
 	isActive := false
-	ticker := time.NewTicker(50 * time.Millisecond)
 	for{
-	select{
-	case message := <- messageChan:
-		switch message.Response{
-		case cl.InnerOrder:
-			content := message.Content.(map[string]interface{})	
-			floor := int(content["Floor"].(float64))
-			sys.AssignOrder(message.Sender,floor)			
-		case cl.OuterOrder:
-			content := message.Content.(map[string]interface{})	
-			floor := int(content["Floor"].(float64))
-			direction := int(content["Direction"].(float64))
-			sys.AddOuterOrder(floor,direction)
-		case cl.Floor:		
-			floor := int(message.Content.(float64))
-			cmd, hasCommand := sys.FloorAction(message.Sender,floor)
-			cmd.Sender = network.LocalIP()
-			cmd.ID = network.CreateID(cl.Master)
-			if hasCommand && isActive{
-				sendMaster <- cmd
+		select{
+		case message := <- messageChan:
+			event := false
+			switch message.Response{
+			case cl.InnerOrder:
+				content := message.Content.(map[string]interface{})	
+				floor := int(content["Floor"].(float64))
+				sys.AssignOrder(message.Sender,floor)
+				event = true			
+			case cl.OuterOrder:
+				content := message.Content.(map[string]interface{})	
+				floor := int(content["Floor"].(float64))
+				direction := int(content["Direction"].(float64))
+				sys.AddOuterOrder(floor,direction)
+				event = true
+			case cl.Floor:		
+				fmt.Println("yoloooo")
+				floor := int(message.Content.(float64))
+				cmd, hasCommand := sys.FloorAction(message.Sender,floor)
+				cmd.Sender = network.LocalIP()
+				cmd.ID = network.CreateID(cl.Master)
+				if hasCommand && isActive{
+					sendMaster <- cmd
+				}
+				event = true
+			case cl.Startup:
+				ping := network.Message{network.LocalIP(),message.Sender,network.CreateID(cl.Master),cl.JoinMaster,""}
+				if isActive{
+					sendMaster <- ping
+				}
+				sys.AddElevator(message.Sender)			
+			case cl.SetMaster:
+				isActive = true
 			}
-		case cl.Startup:
-
-			ping := network.Message{network.LocalIP(),message.Sender,network.CreateID(cl.Master),cl.JoinMaster,""}
-			if isActive{
-				sendMaster <- ping
-			}
-			sys.AddElevator(message.Sender)			
-		case cl.SetMaster:
-			isActive = true
-		}
-	case connStatus := <- statusChan:
-		switch connStatus.Response{
-		case cl.Timeout:
-			sys.RemoveElevator(connStatus.Sender)
-	}
-	case <-ticker.C:
+			if event{
 			cmd, hasCommand := sys.Command()
-			cmd.Sender = network.LocalIP()
-			cmd.ID = network.CreateID(cl.Master)
-			if hasCommand && isActive{
-				fmt.Println("MASTE34R")
-				sendMaster <- cmd
+				cmd.Sender = network.LocalIP()
+				cmd.ID = network.CreateID(cl.Master)
+				if hasCommand && isActive{
+					fmt.Println("MASTE34R")
+					sendMaster <- cmd
+				}
 			}
+		case connStatus := <- statusChan:
+			switch connStatus.Response{
+			case cl.Timeout:
+				sys.RemoveElevator(connStatus.Sender)
+		}
+		/*case <-ticker.C:
+				cmd, hasCommand := sys.Command()
+				cmd.Sender = network.LocalIP()
+				cmd.ID = network.CreateID(cl.Master)
+				if hasCommand && isActive{
+					fmt.Println("MASTE34R")
+					sendMaster <- cmd
+				}
+		*/
 		}
 	}
 			fmt.Println(sys)
